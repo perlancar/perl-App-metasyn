f8f0b5ea2eb6e210ef2de4c7052f2d2e57ef4bba
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-02-17 07:40:39 +0700

    Use Perinci::CmdLine::Inline for less dependency

--- lib/App/metasyn.pm
+++ lib/App/metasyn.pm
@@ -3,8 +3,109 @@ package App::metasyn;
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{metasyn} = {
+    v => 1.1,
+    summary => 'Alternative front-end to Acme::MetaSyntactic',
+    description => <<'_',
+
+This script is an alternative front-end to <pm:Acme::MetaSyntactic>. Compared to
+the official CLI <prog:meta>, this CLI is more oriented towards listing names
+instead of giving you one or several random names.
+
+_
+    args => {
+        action => {
+            schema => ['str*', in=>[qw/list-themes list-names/]],
+            default => 'list-names',
+            cmdline_aliases => {
+                l => { summary => 'List installed themes', is_flag => 1, code => sub { $_[0]{action} = 'list-themes' } },
+            },
+        },
+        theme => {
+            schema => 'str*',
+            pos => 0,
+            completion => sub {
+                require Complete::Acme::MetaSyntactic;
+                Complete::Acme::MetaSyntactic::complete_meta_themes_and_categories(@_);
+            },
+        },
+        shuffle => {
+            schema => ['bool*', is=>1],
+        },
+        categories => {
+            schema => ['bool*', is=>1],
+            cmdline_aliases => {c=>{}},
+        },
+    },
+    examples => [
+        {
+            summary => 'List all installed themes',
+            argv => [qw/-l/],
+        },
+        {
+            summary => 'List all names from a theme',
+            argv => [qw/foo/],
+        },
+        {
+            summary => 'List all names from a theme in random order',
+            argv => [qw(christmas/elf --shuffle)],
+        },
+        {
+            summary => 'List all categories from a theme',
+            argv => [qw(christmas --categories)],
+        },
+    ],
+    links => [
+        {url=>'prog:meta'},
+    ],
+};
+sub metasyn {
+    no strict 'refs';
+    require Acme::MetaSyntactic;
+
+    my %args = @_;
+
+    my $action = $args{action};
+
+    return [200, "OK", [Acme::MetaSyntactic->new->themes]]
+        if $action eq 'list-themes';
+
+    my $theme = $args{theme};
+    return [400, "Please specify theme"] unless $theme;
+    my $cat = $theme =~ s{/(.+)\z}{} ? $1 : undef;
+
+    my $pkg = "Acme::MetaSyntactic::$theme";
+    (my $pkg_pm = "$pkg.pm") =~ s!::!/!g;
+    return [500, "Can't load $pkg: $@"] unless (eval { require $pkg_pm; 1 });
+
+    if ($args{categories}) {
+        my @res;
+        eval { @res = $pkg->categories };
+        #warn if $@;
+        return [200, "OK", \@res];
+    }
+    #my $meta = Acme::MetaSyntactic->new($theme);
+    my @names;
+    if (defined $cat) {
+        @names = @{ ${"$pkg\::MultiList"}{$cat} // [] };
+    } else {
+        @names = @{"$pkg\::List"};
+    }
+    if ($args{shuffle}) {
+        require List::Util;
+        @names = List::Util::shuffle(@names);
+    }
+    return [200, "OK", \@names];
+}
+
 1;
-#ABSTRACT: An alternative front-end to Acme::MetaSyntactic
+#ABSTRACT:
 
 =head1 SYNOPSIS
 
